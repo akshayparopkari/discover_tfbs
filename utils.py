5514068432b47cee00957fa512bdbff445430625
akshayparopkari <aparopkari@ucmerced.edu>
2019-01-25 19:02:43 -0800

    Added parse_fasta() and parse_gff_fasta() functions

--- utils.py
+++ utils.py
@@ -6,13 +6,13 @@
 """
 
 
-def get_blastn_results(f):
+def parse_blastn_results(f):
     """
     Get blastn results in a dict.
     """
-    # import requirements for this function
+    # imports
     from time import localtime, strftime
-    from os.path import join, dirname, basename
+    from os.path import basename
 
     print("{}: PARSING BLASTn RESULTS".format(strftime("%m/%d/%Y %H:%M:%S", localtime())))
     blastn_results = dict()
@@ -64,7 +64,7 @@ def get_blastn_results(f):
                         "send": [int(line[9])],
                         "evalue": [float(line[10])],
                         "bitscore": [float(line[11])],
-                        "strand" :[strand]
+                        "strand": [strand]
                     }
     return blastn_results
 
@@ -74,10 +74,10 @@ def get_kmers(seq, k=6):
     Generate kmers from a given sequence. By default, this function will generate 6-mers
     from the given sequence. Function will return an error if kmer length is greater than
     sequence length.
-    
+
     :type seq: str/list of str
     :param seq: a single nucleotide sequence or a list of nucleotide sequences
-    
+
     :type k: int
     :param k: length of kmers to generate, default is 6mers under the constraint that
               length(seq) > k
@@ -91,12 +91,97 @@ def get_kmers(seq, k=6):
         for s in seq:
             kmers[s] = list()
             for i in range(0, len(s) - (k - 1), 1):
-                kmers[s].append(s[i : i + k])
+                kmers[s].append(s[i: i + k])
         return kmers  # dict of seq and their kmers {sequence: [kmer1, kmer2, ...]}
     else:
         kmers = list()
         # `seq` is a single sequence
         for i in range(0, len(seq) - (k - 1), 1):
-            kmers.append(seq[i : i + k])
+            kmers.append(seq[i: i + k])
         return kmers  # list of kmers [kme1, kmer2, ...]
 
+
+def parse_fasta(fasta_file):
+    """
+    Parse a FASTA file for easy pythonic access.
+
+    :type fasta_file: str
+    :param fasta_file: file path and name handle
+    """
+    # imports
+    from sys import exit
+    from os.path import isfile
+    from pyfaidx import Fasta
+
+    try:
+        assert isfile(fasta_file)
+    except AssertionError:
+        # file doesn't exist
+        exit("\n{} does not exist. Please provide a valid genome FASTA file.\n".
+             format(fasta_file))
+    else:
+        # valid FASTA file
+        return Fasta(fasta_file, one_based_attributes=False)
+
+
+def parse_gff_fasta(gff_file, parsed_fasta, out_fasta="Ca22_CDS_seqs.fasta", genome="22",
+                    feature="CDS"):
+    """
+    Parses a GFF and fasta data object (output from parse_fasta()) of a genome and collect
+    sequences of certain feature in GFF file. By default, this function will return all
+    CDS sequences for all entries in GFF file.
+
+    :type gff_file: str
+    :param gff_file: file path and name handle
+
+    :type fasta_file: dict-like
+    :param fasta_file: Output from parse_fasta()
+
+    :type out_fasta: str
+    :param out_fasta: Output FASTA file path and name. By default, the file name will be
+                     Ca22_CDS_seqs.fasta. I recommend changing file name based on genome
+                     and feature used for your run.
+
+    :type genome: str
+    :param genome: Assembly version for Candida albicans genome. Default value is 22
+                       for Assembly 22, but can be changed to 21 for Assembly 21.
+
+    :type feature: str
+    :param feature: Single feature annotation from input GFF file. Default is 'CDS', but
+                    can be changed by user to other feature types such as 'exon',
+                    'intergenic_region', 'start_codon', 'stop_codon', etc.
+    """
+    # imports
+    from sys import exit
+    from os.path import isfile
+
+    # Testing GFF file
+    try:
+        assert isfile(gff_file)
+    except AssertionError:
+        # file doesn't exist
+        exit("\n{} does not exist. Please provide a valid GFF file.\n".
+             format(gff_file))
+
+    # Go through GFF file and write out feature sequences to output FASTA file
+    with open(gff_file) as infile:
+        with open(out_fasta, "w") as outfile:
+            for line in infile:
+                try:
+                    assert line.startswith("Ca22chr")
+                except AssertionError:
+                    # invalid line, skip processing
+                    continue
+                else:
+                    # valid line, parse contents of line into an object
+                    line = line.strip().split("\t")
+                    start, end = int(line[3]), int(line[4])
+                    try:
+                        assert start < end
+                    except AssertionError:
+                        # feature length is not positive value, skip
+                        continue
+                    else:
+                        outfile.write(">{0}\n{1}\n".format(line[0],
+                                      parsed_fasta[line[0]][start: end].seq))
+    return None
