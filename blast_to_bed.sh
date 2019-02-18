6d7ae5216ed2abbd04e5f9c9f4fb2d8565c99c00
akshayparopkari <aparopkari@ucmerced.edu>
2019-02-17 23:03:03 -0800

    Added comments

--- blast_to_bed.sh
+++ blast_to_bed.sh
@@ -19,17 +19,22 @@
 # Version: 0.0.2
 #########################################################################################
 
+# load Anaconda
 module load anaconda3
 
+# define input and output file variables
 INFILE=$(realpath "$1")
 OUTFILE=$(realpath "$2")
 
+# convert BLAST output to BED format
 echo -e "\nPROCESSING $INFILE"
 awk 'BEGIN {FS="\t"; OFS=FS} {if ($9 < $10) {print $2, $9 - 1, $10, "", "0", "+"} else {print $2, $10 - 1, $9, "", "0", "-"}}' < "$INFILE" > "$OUTFILE"
 echo -e "$INFILE converted to $OUTFILE\n"
 
+# define files for flankBed command
 FLANKEDFILE=$(sed "s/.bed/_1000bp_flanking.bed/" "$OUTFILE")
 CHRFILE=$(realpath "$3")
+
+# get 1000 bp flanking regions around TFBS
 flankBed -i "$OUTFILE" -g "$CHRFILE" -b 1000 -s -header > "$FLANKEDFILE"
 echo -e "BED file with flanking intervals created from $OUTFILE\n"
-
