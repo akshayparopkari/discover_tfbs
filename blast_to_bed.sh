25ed4c9f61f78b7b484e5d53159390d1e472e75f
akshayparopkari <aparopkari@ucmerced.edu>
2019-02-18 13:49:39 -0800

    Few processing updates

--- blast_to_bed.sh
+++ blast_to_bed.sh
@@ -6,7 +6,7 @@
 # with the script.
 #
 # USAGE
-# sh blast_to_bed.sh input_blast_file output_bed_file genome_file
+# sh blast_to_bed.sh input_blast_file genome_file
 #
 # from bedtools reference -
 # The genome file should tab delimited and structured as follows
@@ -15,25 +15,34 @@
 # SCRIPT INFORMATION
 #
 # Author: Akshay Paropkari
-# Version: 0.0.2
+# Version: 0.0.4
 #########################################################################################
 
-# load Anaconda
-module load anaconda3
-
 # define input and output file variables
-INFILE=$(realpath "$1")
-OUTFILE=$(realpath "$2")
+INFILE=$(basename "$1")
+OUTFILE=$(basename "$INFILE" .blout).bed
+CHRFILE=$(realpath "$2")
+
+# change directory to input folder
+if cd "$(dirname "$1")"; then echo -e "\n$(date "+%a %D %r"): cd into input directory successful"; else echo -e "\n$(date "+%a %D %r"): cd into input directory failed. Exiting."; exit 1; fi
 
 # convert BLAST output to BED format
-echo -e "\nPROCESSING $INFILE"
+echo -e "\n$(date "+%a %D %r"): PROCESSING $INFILE"
 awk 'BEGIN {FS="\t"; OFS=FS} {if ($9 < $10) {print $2, $9 - 1, $10, "", "0", "+"} else {print $2, $10 - 1, $9, "", "0", "-"}}' < "$INFILE" > "$OUTFILE"
-echo -e "$INFILE converted to $OUTFILE\n"
+echo -e "\n$(date "+%a %D %r"): $INFILE converted to $OUTFILE"
 
 # define files for flankBed command
-FLANKEDFILE=$(sed "s/.bed/_1000bp_flanking.bed/" "$OUTFILE")
-CHRFILE=$(realpath "$3")
+FLANKEDFILE=$(basename "$OUTFILE" .bed)_1000bp_flanking.bed
+FOLDERNAME="flanking_regions"
 
 # get 1000 bp flanking regions around TFBS
+echo -e "\n$(date "+%a %D %r"): Getting 1000bp flanking interval around TFBS"
 flankBed -i "$OUTFILE" -g "$CHRFILE" -b 1000 -s -header > "$FLANKEDFILE"
-echo -e "BED file with flanking intervals created from $OUTFILE\n"
+if [[ -d "$FOLDERNAME" ]]
+then
+  mv "$FLANKEDFILE" "$FOLDERNAME" 
+else
+  mkdir -p "$FOLDERNAME"
+  mv "$FLANKEDFILE" "$FOLDERNAME"
+fi
+echo -e "\n$(date "+%a %D %r"): BED file with flanking intervals saved in $(realpath "$FOLDERNAME")"
