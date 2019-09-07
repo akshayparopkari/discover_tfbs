e97bcbb816385eac32146ce8262633d600132261
akshayparopkari <aparopkari@ucmerced.edu>
2019-09-06 17:40:01 -0700

    Adding DNA shape features integration

--- build_features.py
+++ build_features.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
 """
-Build feature table  from input FASTA files.
+Build feature table from input FASTA files.
 """
 
 __author__ = "Akshay Paropkari"
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 
 
 from sys import exit
-from os.path import isfile
+from os.path import isfile, abspath
 import argparse
 from collections import defaultdict
 from itertools import product, starmap
 from time import localtime, strftime
+from pprint import pprint
 from utils import parse_fasta, calculate_gc_percent, pac
 err = []
 # try:
@@ -47,15 +48,20 @@ except AssertionError:
 
 
 def handle_program_options():
-    parser = argparse.ArgumentParser(description="Build feature table from input FASTA "
-                                     "files.",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("fg_fasta_file", help="Path to foreground/true positive dataset "
-                        "FASTA format file [REQUIRED]")
-    parser.add_argument("bkg_fasta_file", help="Path to background/negative dataset "
-                        "FASTA format file [REQUIRED]")
-    # parser.add_argument("test_fasta_file", help="Path to test dataset sequences FASTA "
-    #     "format file [REQUIRED]")
+    parser = argparse.ArgumentParser(
+        description="Build feature table from input FASTA files.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+    parser.add_argument("fg_fasta_file", help="Path to foreground/true positive sequence "
+                        "dataset FASTA format file [REQUIRED]")
+    parser.add_argument("bkg_fasta_file", help="Path to background/negative sequence "
+                        "dataset FASTA format file [REQUIRED]")
+    parser.add_argument("-fsff", "--fg_shape_fasta_file", nargs="+", help="Path to 3D DNA"
+                        " shape (DNAShapeR output files) data FASTA format files "
+                        "associated with 'fg_fasta_file' parameters [REQUIRED]")
+    parser.add_argument("-bsff", "--bkg_shape_fasta_file", nargs="+", help="Path to 3D "
+                        "DNA shape (DNAShapeR output files) data FASTA format file "
+                        "associated with 'bkg_fasta_file' parameters [REQUIRED]")
     parser.add_argument("protein_name", type=str, help="Name of transcription factor "
                         "denoted by fasta_file parameter. Name must only contain "
                         "alphanumeric characters [REQUIRED]")
@@ -116,11 +122,11 @@ def main():
     #######################################
     # FOREGROUND SEQUENCE PROCESSING #
     #######################################
-    print("Processing foreground FASTA file", strftime("%x | %X".format(localtime)))
+    print(strftime("%x %X".format(localtime)), ": Processing foreground FASTA file")
     print("="*52, sep="\n")
 
     # get all foreground sequences
-    print("Reading FASTA file: ", strftime("%x | %X".format(localtime)))
+    print(strftime("%x %X".format(localtime)), ": Reading FASTA file")
     fg_seqs = defaultdict(dict)
     fg_seqs[args.protein_name]["header"] = np.asarray([name
                                                        for name, seq in
@@ -130,30 +136,53 @@ def main():
                                                      parse_fasta(args.fg_fasta_file)])
 
     # get GC percent for all foreground sequences
-    print("Calculating GC percent of foreground sequences: ",
-          strftime("%x | %X".format(localtime)))
+    print(strftime("%x %X".format(localtime)), ": Calculating GC percent")
     fg_gc = {tf: map_headers_to_values(data["header"],
                                        list(map(calculate_gc_percent, data["seqs"])))
              for tf, data in fg_seqs.items()}
 
     # calculating poisson based metrics
-    print("Calculating Poisson based metrics", strftime("%x | %X".format(localtime)))
-    seq_pairs = all_possible_seq_pairs(fg_seqs[args.protein_name]["seqs"],
-                                       fg_seqs[args.protein_name]["seqs"])
-    poisson_metrics = np.asarray([np.asarray(list(starmap(pac, pair_set))).
-                                  mean(axis=0, dtype=np.float64)
-                                  for pair_set in seq_pairs])
+    print(strftime("%x %X".format(localtime)), ": Calculating Poisson based metrics")
+    fg_seq_pairs = all_possible_seq_pairs(fg_seqs[args.protein_name]["seqs"],
+                                          fg_seqs[args.protein_name]["seqs"])
+    fg_poisson_metrics = np.asarray([np.asarray(list(starmap(pac, pair_set))).
+                                     mean(axis=0, dtype=np.float64)
+                                     for pair_set in fg_seq_pairs])
 
-    fg_pac = map_headers_to_values(fg_seqs[args.protein_name]["header"], poisson_metrics)
+    fg_pac = map_headers_to_values(fg_seqs[args.protein_name]["header"],
+                                   fg_poisson_metrics)
 
-    #######################################
+    # collate all DNA shape values
+    print(strftime("%x %X".format(localtime)), ": Processing DNA shape data\n")
+    fg_shapes = dict()
+    for shapefile in args.fg_shape_fasta_file:
+        whichshape = shapefile.split(".")[-1]
+        if whichshape in ["MGW", "ProT", "EP"]:
+            for name, shape in parse_fasta(abspath(shapefile)):
+                shape = shape.split(",")
+                if not fg_shapes.get(name):
+                    fg_shapes[name] = dict()
+                for i in range(2, len(shape) - 2):
+                    position = "{0}_pos_{1}".format(whichshape, i + 1)
+                    fg_shapes[name][position] = float(shape[i])
+        else:
+            # shape is Roll or HelT
+            for name, shape in parse_fasta(abspath(shapefile)):
+                shape = shape.split(",")
+                if not fg_shapes.get(name):
+                    fg_shapes[name] = dict()
+                for i in range(1, len(shape) - 1):
+                    position = "{0}_pos_{1}".format(whichshape, i + 1)
+                    fg_shapes[name][position] = float(shape[i])
+
+    ##################################
     # BACKGROUND SEQUENCE PROCESSING #
-    #######################################
-    print("\nProcessing background FASTA file", strftime("%x | %X".format(localtime)))
+    ##################################
+    print(strftime("%x %X".format(localtime)), ": Processing background FASTA file")
     print("="*52, sep="\n")
 
     # get all background sequences
-    print("Reading FASTA file: ", strftime("%x | %X".format(localtime)))
+    print(strftime("%x %X".format(localtime)), ": Reading FASTA file")
     bkg_seqs = defaultdict(dict)
     bkg_seqs[args.protein_name]["header"] = np.asarray([name
                                                        for name, seq in
@@ -163,11 +192,44 @@ def main():
                                                      parse_fasta(args.bkg_fasta_file)])
 
     # get GC percent for all background sequences
-    print("Calculating GC percent of foreground sequences: ",
-          strftime("%x | %X".format(localtime)))
+    print(strftime("%x %X".format(localtime)), ": Calculating GC percent")
     bkg_gc = {tf: map_headers_to_values(data["header"],
                                         list(map(calculate_gc_percent, data["seqs"])))
               for tf, data in bkg_seqs.items()}
+
+    # calculating poisson based metrics
+    print(strftime("%x %X".format(localtime)), ": Calculating Poisson based metrics")
+    bkg_seq_pairs = all_possible_seq_pairs(bkg_seqs[args.protein_name]["seqs"],
+                                           bkg_seqs[args.protein_name]["seqs"])
+    bkg_poisson_metrics = np.asarray([np.asarray(list(starmap(pac, pair_set))).
+                                      mean(axis=0, dtype=np.float64)
+                                      for pair_set in bkg_seq_pairs])
+
+    bkg_pac = map_headers_to_values(bkg_seqs[args.protein_name]["header"],
+                                    bkg_poisson_metrics)
+
+    # collate all DNA shape values
+    print(strftime("%x %X".format(localtime)), ": Processing DNA shape data\n")
+    bkg_shapes = dict()
+    for shapefile in args.bkg_shape_fasta_file:
+        whichshape = shapefile.split(".")[-1]
+        if whichshape in ["MGW", "ProT", "EP"]:
+            for name, shape in parse_fasta(abspath(shapefile)):
+                shape = shape.split(",")
+                if not bkg_shapes.get(name):
+                    bkg_shapes[name] = dict()
+                for i in range(2, len(shape) - 2):
+                    position = "{0}_pos_{1}".format(whichshape, i + 1)
+                    bkg_shapes[name][position] = float(shape[i])
+        else:
+            # shape is Roll or HelT
+            for name, shape in parse_fasta(abspath(shapefile)):
+                shape = shape.split(",")
+                if not bkg_shapes.get(name):
+                    bkg_shapes[name] = dict()
+                for i in range(1, len(shape) - 1):
+                    position = "{0}_pos_{1}".format(whichshape, i + 1)
+                    bkg_shapes[name][position] = float(shape[i])
 
 
 if __name__ == "__main__":
