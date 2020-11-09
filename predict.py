cdf492d0e28e4b95c5482e675fe81162f63044c1
akshayparopkari <akshayparopkari@users.noreply.github.com>
2020-11-09 13:52:23 -0800

    Added SVM hyperplane distance calculation feature

--- predict.py
+++ predict.py
@@ -6,21 +6,28 @@ binding site.
 """
 
 __author__ = "Akshay Paropkari"
-__version__ = "0.1.8"
+__version__ = "0.3.4"
 
 
 import argparse
-from collections import defaultdict
-from itertools import product, starmap
+from itertools import product
 from os.path import isfile
 from sys import exit
 from time import strftime
 
 from joblib import load
-
-from utils import calculate_gc_percent, get_shape_data, pac, parse_fasta
+from utils import build_feature_table, parse_fasta
 
 err = []
+try:
+    import numpy as np
+    from numpy.linalg import norm
+except ImportError:
+    err.append("numpy")
+try:
+    import pandas as pd
+except ImportError:
+    err.append("pandas")
 try:
     import matplotlib as mpl
     from matplotlib import pyplot as plt
@@ -28,14 +35,6 @@ try:
     plt.switch_backend("agg")
 except ImportError:
     err.append("matplotlib")
-try:
-    import numpy as np
-except ImportError:
-    err.append("numpy")
-try:
-    import pandas as pd
-except ImportError:
-    err.append("pandas")
 try:
     assert len(err) == 0
 except AssertionError:
@@ -120,16 +119,19 @@ def handle_program_options():
         "format [REQUIRED]",
     )
     parser.add_argument(
-        "plot_feature_contribution",
+        "model_stats",
         type=str,
-        metavar="/path/to/feature_contribution.pdf",
-        help="Specify location and name of the file to save the contribution "
-        "of all feature towards classification. By default, the plots will be saved in "
-        "the format specified in the file ending by the user. E.g. the "
-        "'feature_contribution.pdf' file will be saved as PDF "
-        "file. For more information about file types, please read the "
-        "'format' attribute of figure.savefig function on matplotlib's "
-        "documentation [REQUIRED]",
+        metavar="/path/to/tf_model_stats/folder",
+        help="Specify location and name of folder to save SVM model statistics "
+        "like distance of hyperplane and log probability of classification [REQUIRED]",
+    )
+    parser.add_argument(
+        "roc_like_curve",
+        type=str,
+        metavar="/path/to/roc_like_curve.pdf",
+        help="Specify location and name of the file to save the ROC-like curve "
+        "of positive classification. X-axis is positive TFBS predictions and Y-axis "
+        "denotes true TFBS [REQUIRED]",
     )
     return parser.parse_args()
 
@@ -168,48 +170,12 @@ def all_possible_seq_pairs(list1, fg_seqs):
     :type fg_seqs: array-like
     :param fg_seqs: Array of foreground sequences
     """
-    return (list(product([seq], fg_seqs)) for seq in list1)
-
-
-def plot_coefficients(coef, feature_names, file: str):
-    """
-    Using the coefficient weights, plot the contribution of each feature in
-    classification. Currently, this function is set up for binary classification.
-
-    :type coef: array-like, list or numpy array
-    :param coef: SVM weights assigned to each feature.
-
-    :type names: list
-    :param names: List of feature names to use for plotting
-
-    :type file: str
-    :param file: Path and name of file to save feature contribution bar plot. The file
-                 will be saved in PDF format.
-    """
-    coef = coef.ravel()
-    feature_names = np.array(feature_names)
-    top_coefficients = np.argsort(coef)
-
-    # create plot
-    with mpl.style.context("fast"):
-        plt.figure(figsize=(10, 10))
-        colors = [
-            "#fc4f00" if c < 0 else "#aaa000" for c in coef[top_coefficients][::-1]
-        ]
-        plt.barh(
-            range(len(coef)),
-            coef[top_coefficients][::-1],
-            color=colors,
-            hatch="//",
-            tick_label=feature_names[top_coefficients][::-1],
-        )
-        plt.grid(ls=":")
-        plt.savefig(file, dpi=300, format="pdf", bbox_inches="tight")
+    return (list(product(fg_seqs, [seq])) for seq in list1)
 
 
 def main():
 
-    print("#" * 90, strftime("%x %X | START CLASSIFICATION\n"), sep="\n\n")
+    print("#" * 90, "\n\n", strftime("%x %X | START CLASSIFICATION\n"))
     args = handle_program_options()
 
     # Check input validity
@@ -224,80 +190,50 @@ def main():
         print("Error with input file(s). Please review the error\n{0}".format(e))
         exit()
     else:
-        file_formats = ["pdf", "svg", "png", "jpg", "tiff", "eps", "ps"]
-        output_format = args.plot_feature_contribution.split("/")[-1].split(".")[-1]
+        protein_name = args.protein_name.capitalize()
         try:
-            assert output_format in file_formats
+            output_format = args.roc_like_curve.split("/")[-1].split(".")[-1]
+            assert output_format in ["pdf", "svg", "png", "jpg", "tiff", "eps", "ps"]
         except AssertionError:
             print(
-                "\nError: Please check the output file format provided. '{0}' format is "
-                "not supported in {1}.".format(
-                    output_format, args.plot_feature_contribution
-                )
+                f"Error: Please check the output file format provided. '{output_format}' format is not supported in {args.roc_like_curve}."
             )
-        protein_name = args.protein_name.capitalize()
 
         ######################################
         # Read in training data feather file #
         ######################################
         try:
-            training_data = pd.read_feather(args.read_training_data)
+            training_data = (
+                pd.read_feather(args.read_training_data)
+                .drop(columns=["index"])
+                .set_index("location", verify_integrity=True)
+            )
         except Exception:
             print("Error: Please check input file {0}".format(args.read_training_data))
             exit()
         else:
             # feather file reading successful, collect feature set
+            # starting at 2, since location is now row index
             training_features = list(training_data.columns[2:])
+            X_train = training_data.iloc[:, 2:].to_numpy()
 
         ######################
-        # Read in FASTA file #
+        # Read in shape data #
         ######################
-        prediction_data = defaultdict(str)
-        prediction_data = {
-            name: seq for name, seq in parse_fasta(args.prediction_FASTA)
+        shape_data = {
+            file.split(".")[-1]: {
+                header: seq.strip().split(",") for header, seq in parse_fasta(file)
+            }
+            for file in args.genome_wide_shape_fasta_file
         }
 
-    ####################################################
-    # Calculating GC content and poisson based metrics #
-    ####################################################
-    print(strftime("%x %X | Calculating GC percent"))
-    pred_gc = map_headers_to_values(
-        prediction_data.keys(),
-        np.fromiter(map(calculate_gc_percent, list(prediction_data.values())), float),
-    )
+    ##########################################################
+    # Calculating GC content and sequence similarity metrics #
+    ##########################################################
+    prediction_data_df = build_feature_table(
+        args.prediction_FASTA, args.fg_fasta_file, shape_data, minhash=True
+    ).set_index("location", verify_integrity=True)
 
-    print(strftime("%x %X | Calculating Poisson based metrics"))
-    fg_seqs = [seq for header, seq in parse_fasta(args.fg_fasta_file)]
-    prediction_data_pairs = all_possible_seq_pairs(prediction_data.values(), fg_seqs)
-    pred_poisson_metrics = np.asarray(
-        [
-            np.asarray(list(starmap(pac, pair_set))).mean(axis=0, dtype=np.float64)
-            for pair_set in prediction_data_pairs
-        ]
-    )
-    pred_pac = map_headers_to_values(prediction_data.keys(), pred_poisson_metrics)
-
-    #####################################################
-    # Calculating 3d DNA shape data for input sequences #
-    #####################################################
-    print(strftime("%x %X | Calculating DNA shape data"))
-    pred_shapes = get_shape_data(args.prediction_BED, args.genome_wide_shape_fasta_file)
-
-    #############################
-    # Collect data in DataFrame #
-    #############################
-    print(strftime("%x %X | Creating prediction dataset"))
-    gc_data_df = pd.DataFrame.from_dict(pred_gc, orient="index", columns=["GC_percent"])
-    pac_data_df = pd.DataFrame.from_dict(
-        pred_pac, orient="index", columns=["PAS", "PPS"]
-    )
-    shapes_data_df = pd.DataFrame.from_dict(pred_shapes, orient="index")
-    prediction_data_df = gc_data_df.merge(
-        pac_data_df, how="outer", left_index=True, right_index=True
-    )
-    prediction_data_df = prediction_data_df.merge(
-        shapes_data_df, how="outer", left_index=True, right_index=True
-    )
     try:
         prediction_data_df = prediction_data_df.loc[:, training_features]
     except KeyError as ke:
@@ -305,27 +241,25 @@ def main():
         prediction_data_features = list(prediction_data_df.columns)
         print(
             strftime(
-                "%x %X | Some feature labels were not found in prediction "
-                "dataset\n{0}".format(ke)
+                f"%x %X | Some feature labels were not found in prediction dataset\n{ke}"
             )
         )
         if len(prediction_data_features) > len(training_features):
             additional_features = set(prediction_data_features).difference(
                 training_features
             )
-            print("Features model is unaware of -\n{0}".format(additional_features))
+            print(f"Features model is unaware of -\n{additional_features}")
+            exit()
         else:
             missing_features = set(training_features).difference(
                 prediction_data_features
             )
-            print("Missing features -\n{0}".format(missing_features))
+            print(f"Missing features -\n{missing_features}")
             exit()
     else:
         print(
             strftime(
-                "%x %X | Saving feature table (input for model) in {0}".format(
-                    args.save_feature_table
-                )
+                f"%x %X | Saving feature table (input for model) in {args.save_feature_table}"
             )
         )
         save_predict_data = prediction_data_df.reset_index()
@@ -340,7 +274,6 @@ def main():
 
         original_row_count = prediction_data_df.shape[0]
         prediction_data_df = prediction_data_df.dropna()
-        #         new_rows = prediction_data_df.shape[0]
         lost_entries_pct = 100 * (len(dropped_entries) / original_row_count)
         print(
             strftime(
@@ -351,11 +284,14 @@ def main():
         )
         prediction_data_features = prediction_data_df.to_numpy()
         model = load(args.model_file)
-        prediction_data_features = model["scaler"].transform(prediction_data_features)
+        prediction_data_features = (
+            model["scaler"].fit(X_train).transform(prediction_data_features)
+        )
+        X_train_transformed = model["scaler"].fit_transform(X_train)
 
-        ############################################
-        # Classify sequences as True vs False TFBS #
-        ############################################
+        ###################################################
+        # Classify sequences as either True or False TFBS #
+        ###################################################
         print(strftime("%x %X | Classifying sequences"))
         clf = model["search"].best_estimator_
         try:
@@ -366,6 +302,155 @@ def main():
             positive_pred_orfs = prediction_data_df.index.to_numpy()[
                 np.where(pred_results)
             ]
+            print(
+                strftime(
+                    f"%x %X | Model predicted {len(positive_pred_orfs):,}{len(positive_pred_orfs) / len(prediction_data_df.index): .2%} positive TFBS hits"
+                )
+            )
+            ######################################
+            # Calculate distance from hyperplane #
+            ######################################
+            try:
+                prediction_decision = np.asarray(
+                    clf.decision_function(prediction_data_features) / norm(clf.coef_)
+                )
+                training_decision = np.asarray(
+                    clf.decision_function(X_train_transformed) / norm(clf.coef_)
+                )
+            except Exception:
+                print(
+                    f"{'': >20}Error while calculating exact distance from hyperplane. "
+                    "Reverting to using relative distance from hyperplane."
+                )
+                prediction_decision = np.asarray(
+                    clf.decision_function(prediction_data_features)
+                )
+                training_decision = np.asarray(
+                    clf.decision_function(X_train_transformed)
+                )
+
+            print(
+                strftime(f"%x %X | Saving SVM model statistics to {args.model_stats}")
+            )
+            training_model_stats = pd.DataFrame.from_records(
+                np.column_stack(
+                    [
+                        training_data.index,
+                        clf.predict(X_train_transformed),
+                        training_decision,
+                        clf.predict_log_proba(X_train_transformed),
+                    ]
+                ),
+                columns=[
+                    "location",
+                    "prediction",
+                    "distance_from_hyperplane",
+                    "log_probability_of_True_TFBS",
+                    "log_probability_of_False_TFBS",
+                ],
+            ).set_index("location", verify_integrity=True)
+            fnh = f"{args.model_stats}{args.protein_name}_training_model_statistics.txt"
+            training_model_stats.to_csv(f"{fnh}", sep="\t", na_rep="NA")
+            prediction_model_stats = pd.DataFrame.from_records(
+                np.column_stack(
+                    [
+                        prediction_data_df.index,
+                        pred_results,
+                        prediction_decision,
+                        clf.predict_log_proba(prediction_data_features),
+                    ]
+                ),
+                columns=[
+                    "location",
+                    "prediction",
+                    "distance_from_hyperplane",
+                    "log_probability_of_True_TFBS",
+                    "log_probability_of_False_TFBS",
+                ],
+            ).set_index("location", verify_integrity=True)
+            fnh = (
+                f"{args.model_stats}{args.protein_name}_prediction_model_statistics.txt"
+            )
+            prediction_model_stats.to_csv(f"{fnh}", sep="\t", na_rep="NA")
+
+            # Collect positive training and prediction data
+            positive_pred_data = prediction_decision[
+                np.where(prediction_decision > 0.0)
+            ]
+            positive_train_data = training_decision[np.where(training_decision > 0.0)]
+
+            # Collect high-confidence positive TFBS prediction
+            min_dist_true_TFBS_from_hyperplane = min(
+                training_model_stats["distance_from_hyperplane"][
+                    training_model_stats["prediction"] == 1
+                ].tolist()
+            )
+            positive_high_confidence_pred_data = prediction_decision[
+                np.where(prediction_decision > min_dist_true_TFBS_from_hyperplane)
+            ]
+
+            ########################################################################
+            # Plot ROC-like curves for true positive and positively predicted data #
+            ########################################################################
+            max_dist = max(
+                np.r_[positive_high_confidence_pred_data, positive_train_data]
+            )
+            new_predictions = np.fromiter(
+                [
+                    np.count_nonzero(positive_high_confidence_pred_data <= dist)
+                    for dist in np.linspace(
+                        min_dist_true_TFBS_from_hyperplane, max_dist, 100
+                    )
+                ],
+                "int32",
+            )
+            # transform new_predictions for cleaner axes
+            new_predictions = np.sqrt(new_predictions)
+            new_predictions = np.true_divide(new_predictions, new_predictions.max())
+            true_predictions = np.fromiter(
+                [
+                    np.count_nonzero(positive_train_data <= dist)
+                    for dist in np.linspace(
+                        min_dist_true_TFBS_from_hyperplane, max_dist, 100
+                    )
+                ],
+                "int32",
+            )
+            # transform true_predictions for cleaner axes
+            true_predictions = np.sqrt(true_predictions)
+            true_predictions = np.true_divide(true_predictions, true_predictions.max())
+
+            print(strftime(f"%x %X | Saving ROC-like plot to {args.roc_like_curve}"))
+            with mpl.style.context("fast"):
+                plt.figure(figsize=(7, 7), edgecolor="k", tight_layout=True)
+                plt.step(
+                    new_predictions, true_predictions, lw=2, alpha=1, where="post",
+                )
+                plt.fill_between(
+                    new_predictions, true_predictions, alpha=0.5, step="post"
+                )
+                plt.xlabel("Positive Predictions", color="k", size=20)
+                plt.ylabel("True Positives", color="k", size=20)
+                plt.figtext(
+                    0.2,
+                    0.935,
+                    f"{protein_name}",
+                    c="w",
+                    backgroundcolor="k",
+                    size=20,
+                    weight="bold",
+                    ha="center",
+                    va="center",
+                )
+                plt.tight_layout()
+                plt.savefig(
+                    args.roc_like_curve,
+                    dpi=300.0,
+                    format=output_format,
+                    edgecolor="k",
+                    bbox_inches="tight",
+                    pad_inches=0.1,
+                )
 
         ###########################################################
         #        Save positive predictions in BED format          #
@@ -373,9 +458,7 @@ def main():
         ###########################################################
         print(
             strftime(
-                "%x %X | Writing positive prediction results to {0}".format(
-                    args.prediction_results
-                )
+                f"%x %X | Writing positive prediction results to {args.prediction_results}"
             )
         )
         with open(args.prediction_results, "w") as pred_out:
@@ -391,22 +474,6 @@ def main():
                         chrom, chromStart, chromEnd, pred_name, score, strand
                     )
                 )
-
-        ###############################################
-        # Feature contribution towards classification #
-        ###############################################
-        print(
-            strftime(
-                "%x %X | Saving feature importance ranking plot to {0}".format(
-                    args.plot_feature_contribution
-                )
-            )
-        )
-        feature_names = [
-            entry.replace("_", " ").replace("pos ", "Pos")
-            for entry in prediction_data_df.columns.to_numpy()
-        ]
-        plot_coefficients(clf.coef_, feature_names, args.plot_feature_contribution)
 
         print(strftime("\n%x %X | END CLASSIFICATION\n"), sep="\n")
 
